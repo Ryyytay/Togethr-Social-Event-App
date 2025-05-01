20c78ff5a66822ff5141a45e99cda8e1300dc9ce
Ryantayy <taypeng1214@gmail.com>
2025-05-01 18:36:54 +1200

    API controller

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c59f73d252ee3aa9a2c3ef9a0b327cd7a5c2ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3923f9cb83ee224285104ad191042c5ef53d5f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
