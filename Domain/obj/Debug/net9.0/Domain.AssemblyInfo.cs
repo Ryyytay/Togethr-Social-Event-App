7d6fe136b3476efcc2594402b58d0da95407b3b4
Ryantayy <taypeng1214@gmail.com>
2025-05-08 20:25:53 +1200

    Creating basic CRUD

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530626b15db16c706a09f0f607df3bf9777061db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faec32745e694ef24a253a5ae0109c67fefc2c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
