793260990f71af37d0698714db29d5527f6551bf
Ryantayy <taypeng1214@gmail.com>
2025-05-17 16:59:55 +1200

    update routing

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8126f250a8a0993729fa05d60ed25deaf5cf9ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1bd2d9f990406769d0970ac1c8070d934b05e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
