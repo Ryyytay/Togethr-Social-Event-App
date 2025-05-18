19ca59f36f4cb934d26b01f51e329b592954ecaa
Ryantayy <taypeng1214@gmail.com>
2025-05-18 20:08:25 +1200

    Styling User Interface

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1bd2d9f990406769d0970ac1c8070d934b05e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a475a0661b2828f725770edae8c31da2fb4d2652")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
