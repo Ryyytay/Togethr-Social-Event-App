1efa03279654c03a70c6559af293e7d9792580eb
Ryantayy <Ryan.TayPengYeow@lincolnuni.ac.nz>
2025-05-19 13:38:28 +1200

    Add loading state

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,14 +9,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { router } from './app/router/Routes.tsx';
 import { RouterProvider } from 'react-router';
+import { store, StoreContext } from './lib/stores/store.ts';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools/>
-        <RouterProvider router={router}/>
-    </QueryClientProvider>
+    <StoreContext.Provider value={store}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools/>
+          <RouterProvider router={router}/>
+      </QueryClientProvider>
+    </StoreContext.Provider>
   </StrictMode>,
 )
