844df7497a60c1512ef18168c053b81da4717c94
Ryyytay <taypeng1214@gmail.com>
2025-06-10 21:51:02 +1200

    Added About page, delete photo function

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f5d3c81e951130a46fdeaedc93408b99965682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b5acc29eb455a35938c1132fc884950a3a5ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
