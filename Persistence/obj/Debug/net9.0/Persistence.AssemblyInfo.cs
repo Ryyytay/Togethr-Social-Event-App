4525d301554646b019b2fcb9bda9327816f31319
Ryyytay <taypeng1214@gmail.com>
2025-06-12 21:27:03 +1200

    Setup SignalR for the live chat in the event

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49145a3b9f57bb8c58cd7286c0d0a30a665735c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a938d248e557df1db6060605798998895f735b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
