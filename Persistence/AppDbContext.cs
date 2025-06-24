98390e87830bc9418fe6d3e3efdd70d49a506140
Ryyytay <taypeng1214@gmail.com>
2025-06-24 19:48:36 +1200

    Minor bug fix

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -50,7 +50,7 @@ namespace Persistence
                 x.HasOne(o => o.Target)
                     .WithMany(f => f.Followers)
                     .HasForeignKey(o => o.TargetId)
-                    .OnDelete(DeleteBehavior.Cascade);
+                    .OnDelete(DeleteBehavior.NoAction);
             });
 
             var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
